02d16ae420ec2b1594ae9be7eacabbcf06812d45
Adi Muraru <amuraru@adobe.com>
2020-05-16 21:36:10 +0300

    Upload file in_thread support

--- slackbot/dispatcher.py
+++ slackbot/dispatcher.py
@@ -318,6 +318,27 @@ class Message(object):
             channel=self._body['channel'],
             timestamp=self._body['ts'])
 
+    @unicode_compact
+    def reply_upload_file(self, text, in_thread=None):
+        """
+            Upload the file and send it as a reply
+            If the message was send in a thread, answer in a thread per default.
+        """
+        if in_thread is None:
+            in_thread = 'thread_ts' in self.body
+
+        if in_thread:
+            thread_ts = self.thread_ts
+        else:
+            thread_ts = None
+        self._client.upload_file(
+            self._body['id'],
+            to_utf8(fname),
+            to_utf8(fpath),
+            to_utf8(initial_comment),
+            thread_ts=thread_ts
+        )
+
     @property
     def channel(self):
         return self._client.get_channel(self._body['channel'])
